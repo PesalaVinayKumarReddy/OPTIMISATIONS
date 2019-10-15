8788c6e75d62172545c85d8f5d0c1a02ce8056dd
PesalaVinayKumarReddy <56136359+PesalaVinayKumarReddy@users.noreply.github.com>
2019-10-15 15:11:06 +0530

    Add files via upload

--- gradvinay.py
+++ gradvinay.py
@@ -7,7 +7,7 @@ Created on Mon Oct 14 10:46:56 2019
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
-learningrate=0.01
+learningrate=0.001
 x=np.asmatrix(np.linspace(-34,34,69))
 L=np.subtract(np.multiply(np.subtract(x,1),np.sin(np.pi/7)),np.multiply(18,np.cos(np.pi/7)))
 data = np.transpose(np.multiply(120,np.divide(L,np.add(np.square(np.subtract(x,1)),18**2))))
@@ -15,24 +15,32 @@ M=119.99995139785717
 K=0.4607158569739635
 x0=1.7559713848807337
 z0=18.677372743366423
-for i in range(0,1):
+for i in range(0,100):
     L1=np.transpose(x)-np.multiply(x0,np.ones((69,1)))
     L1=np.subtract(np.multiply(L1,K),np.multiply(z0,np.sqrt(np.subtract(1,np.square(K)))))
     L2=np.add(np.square(np.transpose(x)-np.multiply(x0,np.ones((69,1)))),np.square(z0))
     F = np.multiply(M,np.divide(L1,L2))
     F1 = np.sum(np.square(data-F),axis=0)
-    dK1 = np.divide(np.subtract(x,x0),np.power(np.add(np.square(np.subtract(x,x0)),z0*z0),1.5))
-    dK2 = np.divide(z0*K/((1-K*K)**0.5),np.power(np.add(np.square(np.subtract(x,x0)),z0*z0),1.5))
+    det=np.sqrt(np.power(np.add(np.square(np.subtract(x,x0)),z0*z0),3))
+    det2=np.sqrt(np.power(np.add(np.square(np.subtract(x,x0)),z0*z0),5))
+    det3=np.sqrt(1-K*K)
+    dK1 = np.divide(np.subtract(x,x0),det)
+    dK2 = np.divide(z0*K/(det3),det)
     dK3 = np.multiply(dK1+dK2,M)
     dK=np.dot(dK3,data-F)/69
-    dM=np.sum(np.divide(F,M),axis=0)
-    dx01=np.divide(np.multiply(3*z0*((1-K*K)**0.5),np.subtract(x,x0)),np.power(np.add(np.square(np.subtract(x,x0)),z0*z0),2.5))
-    dx02=np.divide(np.multiply(3*K,np.square(np.subtract(x,x0))),np.power(np.add(np.square(np.subtract(x,x0)),z0*z0),2.5))
-    dx03=np.divide(-K,np.power(np.add(np.square(np.subtract(x,x0)),z0*z0),1.5))
+    dM=np.divide(F,M)
+    dM=np.dot(np.reshape(dM,(1,69)),data-F)/69
+    dx01=np.divide(np.multiply(-3*z0*(det3),np.subtract(x,x0)),det2)
+    dx02=np.divide(np.multiply(3*K,np.square(np.subtract(x,x0))),det2)
+    dx03=np.divide(-K,det)
     dx0=np.multiply(M,dx01+dx02+dx03)
-    dz01=np.divide(np.multiply(-3*z0*K,np.subtract(x,x0)),np.power(np.add(np.square(np.subtract(x,x0)),z0*z0),2.5))
-    dz02=np.divide(np.multiply(-3*z0*z0*((1-K*K)**0.5),np.power(np.add(np.square(np.subtract(x,x0)),z0*z0),2.5))
-    dz03=np.divide(-((1-K*K)**0.5),np.power(np.add(np.square(np.subtract(x,x0)),z0*z0),1.5))
-    x0=x0-dx0*learningrate
-    M=M-dM*learningrate
-    K=K-dK*learningrate
+    dx0=np.dot(dx0,data-F)/69
+    dz01=np.divide(np.multiply(-3*z0*K,np.subtract(x,x0)),det2)
+    dz02=np.divide(3*z0*z0*(det3),det2)
+    dz03=np.divide(-(det3),det)
+    dz0=np.multiply(M,dz01+dz02+dz03)
+    dz0=np.dot(dz0,data-F)/69
+    z0=z0-dz0*learningrate*1
+    x0=x0-dx0*learningrate*100
+    M=M-dM*learningrate*0.1
+    K=K-dK*learningrate*0.1
